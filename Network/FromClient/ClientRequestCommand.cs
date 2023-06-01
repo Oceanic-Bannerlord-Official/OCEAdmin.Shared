ede708a2931a22f2554102f34a91fe75ee74a3e4
zacharyenriquee <zacharyenriquee@gmail.com>
2023-06-02 03:46:54 +1000

    ADDITION: ClientRequestCommand packet for executing virtual commands on the admin panel

---
+++ Network/FromClient/ClientRequestCommand.cs
@@ -0,0 +1,25 @@
+﻿using OCEAdmin.Shared.Network.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCEAdmin.Shared.Network.FromClient
+{
+    [PacketId(14)]
+    public class ClientRequestCommand : Packet
+    {
+        public string command;
+        public string[] args;
+
+        public new int packetId = 14;
+
+        public ClientRequestCommand() { }
+
+        public ClientRequestCommand(string command)
+        {
+            this.command = command;
+        }
+    }
+}
