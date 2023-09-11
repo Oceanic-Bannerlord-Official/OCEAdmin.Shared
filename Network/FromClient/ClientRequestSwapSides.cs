4005b0cd3f58c2dd45d55e65228e29c91be700be
zacharyenriquee <zacharyenriquee@gmail.com>
2023-09-11 23:02:58 +1000

    UPDATE: Misc panel stuff

---
+++ Network/FromClient/ClientRequestSwapSides.cs
@@ -0,0 +1,17 @@
+﻿using OCEAdmin.Shared.Network.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCEAdmin.Shared.Network.FromClient
+{
+    [PacketId(15)]
+    public class ClientRequestSwapSides : Packet
+    {
+        public new int packetId = 15;
+
+        public ClientRequestSwapSides() { }
+    }
+}
